8c17fe1b68136434a80092be567747042554bf36
Snow-Hash-Code <gerardoluis206@gmail.com>
2026-05-16 20:32:58 -0600

    fix: reducir tamaño del middleware para Vercel free plan

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,13 @@
-import { auth } from '@/auth'
-import { NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+import { NextRequest, NextResponse } from 'next/server'
 
-export default auth((req) => {
-  const isLoggedIn = !!req.auth
+export async function middleware(req: NextRequest) {
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  })
+
+  const isLoggedIn = !!token
   const isAuthPage =
     req.nextUrl.pathname.startsWith('/login') ||
     req.nextUrl.pathname.startsWith('/register')
@@ -17,7 +22,7 @@ export default auth((req) => {
   }
 
   return NextResponse.next()
-})
+}
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
